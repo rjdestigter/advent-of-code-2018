f888533a9b383073a74ff207aab459c18d56651a
rjdestigter <johndestigter@gmail.com>
2018-12-03 20:26:09 -0700

    Solution for Day 1

--- src/One.hs
+++ src/One.hs
@@ -1,10 +1,15 @@
 module One where
 
-run :: IO Int
-run = do
+import Data.List (find)
+import Debug.Trace
+
+dayOne :: IO (Int, Int)
+dayOne = do
     linesOfFile <- readInputsFromFile
     let fns = inputLinesToListOfFn linesOfFile
-    return $ sumListOfFn fns
+    let sum = sumListOfFn fns
+    let freq = findFrequency fns
+    return (freq, sum)
 
 plus :: Int -> Int -> Int
 plus b a = a + b
@@ -12,12 +17,16 @@ plus b a = a + b
 minus :: Int -> Int -> Int
 minus b a = a - b
 
+-- Reads the input file provided by adventofcode.com
 readInputsFromFile :: IO [String]
 readInputsFromFile = do
-    inputFileContents <- readFile "./assets/one.txt"
-    let linesOfFile = lines inputFileContents
-    return linesOfFile
+    -- Read file contents
+    inputFileContents <- readFile "./src/inputs/one.txt"
+    -- Split into lines
+    return (lines inputFileContents)
 
+-- Converts each line into a partially applied function
+-- So "+3" bcomes a function that takes a number and adds 3 to it
 inputLine2Fn :: String -> (Int -> Int)
 inputLine2Fn [] = plus 0
 inputLine2Fn (x:[]) = plus 0
@@ -26,9 +35,31 @@ inputLine2Fn (x:xs) = case x of
     '-' -> minus (read xs)
     _ -> plus 0
 
+-- Converts a list of input lines to a list of partially applied functions
 inputLinesToListOfFn :: [String] -> [Int -> Int]
 inputLinesToListOfFn = fmap inputLine2Fn
 
+-- Sums the list of of partially applied functions starting at applying zero
 sumListOfFn :: [Int -> Int] -> Int
 sumListOfFn = foldr (\a b -> a b) 0
 
+-- Find the first frequency that appears twice
+findFrequency :: [(Int -> Int)] -> Int
+findFrequency operations =
+    
+    go operations [0] 0
+    
+    where
+        go :: [(Int -> Int)] -> [Int] -> Int -> Int
+        go [] frequencies accumulatedFrequency = go operations frequencies accumulatedFrequency
+        go (nextOperation:tailOperations) frequencies accumulatedFrequency =
+            let
+                nextFrequency :: Int
+                nextFrequency = nextOperation accumulatedFrequency
+
+                twiceFrequency :: Maybe Int
+                twiceFrequency = find (\fq -> fq == nextFrequency) frequencies
+            in
+                case twiceFrequency of
+                    Just _ -> nextFrequency
+                    _ -> go tailOperations (frequencies ++ [nextFrequency]) nextFrequency
